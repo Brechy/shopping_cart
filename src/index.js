f9169ee1d0d4700d72e353eac3654e7e00bf8261
Corina Andariese <candariese@gmail.com>
2018-07-19 11:27:07 -0700

    Modified files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/Index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
